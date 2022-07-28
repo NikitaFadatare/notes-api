10df9778fcb95f4b75fc3ee98bc5850237afd4a5
Nikita Fadatare <nikitabalaji@hotmail.com>
2022-07-28 12:07:31 +0530

    Implemented add notes functionality

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 import mongoose from "mongoose";
 
-mongoose.connect("");
+mongoose.connect("mongodb://localhost:27017/nodesdb");
 
 const notesSchema = new mongoose.Schema({
     title: String,
-    description:String,
+    description: String,
 });
 
 const NoteModel = mongoose.model("Notes", notesSchema);
+
+const addNotes = (note) => {
+    const newNote = new NoteModel(note);
+    newNote.save((error, data) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(data);
+        }
+    })
+}
+
+addNotes({
+    title: "Going to attend Node Online class",
+    description: "Conducted by Nikita Balaji"
+});
